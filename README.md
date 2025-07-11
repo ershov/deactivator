a459318674af616a70c4d4f3ef0a60fa697860c8
Yury Ershov <yuriy.ershov@gmail.com>
2025-07-12 07:12:04 +1000

    Convert to Manifest V3

---
+++ README.md
@@ -0,0 +1,19 @@
+CLICK ON THE EXTENSION ICON to sease all scripts activity on the page, thus saving CPU, memory, and network.
+
+Super-lightweight page deactivator in one click.
+
+Tired of a webpage, sucking your CPU in the background?
+But there is something useful in it so you'd like to keep it open?
+Deactivate the web page!
+Cease all scripts activity on the current page, thus saving CPU, memory, and network.
+Also, overall page responsiveness improves making if feel faster.
+
+Works somewhat similar to 'Tab Suspender' but leaves the whole page accessible, just removing all active components and hidden scripts.
+
+In contrast to NoScript-like extensions, works only on demand.
+
+To activate, just click on the extension icon.
+
+TODO:
+* Draw a fancy icon
+* Automatic preventive activity suppressor based on black/white lists
--- background.js
+++ background.js
@@ -1,6 +1,111 @@
 "use strict";
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-   chrome.tabs.executeScript(null, {code: "setTimeout('eval(unescape(\""+BundleScript()+"\"))',50);"});
+// Import the common functions for the service worker
+importScripts('common.js');
+
+chrome.action.onClicked.addListener(async function(tab) {
+   try {
+       await chrome.scripting.executeScript({
+           target: { tabId: tab.id },
+           func: executeDeactivationScript
+       });
+   } catch (error) {
+       console.error('Failed to execute script:', error);
+   }
 });
+
+// Function to be injected into the page
+function executeDeactivationScript() {
+    // Re-define the functions in the page context since we can't access them from the service worker
+    function NodePath(n) {
+        "use strict";
+        return n ?
+            NodePath(n.parentNode) + "/" + n.nodeName + (
+                n.parentNode ?
+                "(" + Array.prototype.indexOf.call(n.parentNode.childNodes, n) + ")" :
+                "") :
+            "";
+    }
+
+    function TraverseDom(element, callback) {
+        "use strict";
+        var stack = [element];
+        while (stack.length > 0) {
+            var node = stack.shift();
+            if (callback(node)) {
+                node.childNodes.forEach((node,i) => {
+                    stack.push(node);
+                });
+            } else {
+                if (node.parentNode) node.parentNode.removeChild(node);
+            }
+        }
+    }
+
+    function RemoveListeners(node) {
+        "use strict";
+        var l = getEventListeners(node);
+        var a;
+        for (a in l) {
+            Array.from(l[a]).forEach(aa=>{
+                console.log(NodePath(node), a, aa);
+                aa.remove();
+            });
+        }
+    }
+
+    function ClearAllTimeouts() {
+        "use strict";
+        var limit = setTimeout(";", 999999);
+        var i;
+        for (i = 0; i <= limit; i++) {
+            clearTimeout(i);
+            clearInterval(i);
+        }
+    }
+
+    function Deactivate() {
+        "use strict";
+        ClearAllTimeouts();
+        document.body.setAttribute("deactivated", "1");
+        TraverseDom(document, (e) => {
+            if (e.nodeName.match(/SCRIPT|OBJECT|EMBED|IFRAME/i)) return false;
+            //RemoveListeners(e);
+            var n;
+            for (n in e) {
+                if (n.match(/^on/)) {
+                    if (e.getAttribute) {
+                        if (e.getAttribute(n)) console.log(NodePath(e), n, e.getAttribute(n));
+                        e.removeAttribute(n);
+                    } else {
+                        if (e[n]) console.log(NodePath(e), n, e[n]);
+                        e[n] = "";
+                        delete e[n];
+                    }
+                }
+            }
+            return true;
+        });
+        Array.from(document.childNodes).forEach(n => {
+            document.replaceChild(n.cloneNode(true), n);
+        });
+    }
+
+    function ShowBadge() {
+        "use strict";
+        if (!document.getElementById("page_inactive_reminder")) {
+            document.body.insertAdjacentHTML("beforeend",
+            "<div id=page_inactive_reminder style='font-size:8px;text-align:center;position:fixed;z-index:1999999999;top:0;width:100%'>(Page is inactive)</div>");
+        }
+        document.body.insertAdjacentHTML("beforeend",
+        "<div id=deactivate_badge style='position:fixed;z-index:1999999999;top:50px;width:100%'><center><mark style='padding:20px'>Page DEACTIVATED</mark></center></div>");
+        setTimeout("(e=>e.parentNode.removeChild(e))(document.getElementById('deactivate_badge'))", 1500);
+    }
+
+    // Execute the deactivation
+    setTimeout(() => {
+        Deactivate();
+        ShowBadge();
+    }, 50);
+}
 
